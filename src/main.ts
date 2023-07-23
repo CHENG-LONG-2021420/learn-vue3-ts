2553f1915c96ea8aa462686ab8887a1f3cd1f217
CHENG-LONG-2021420 <13032212876@163.com>
2023-07-23 15:19:35 +0800

    chore(scss, lint, api): add Environmental Dependence

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+// 引入element-plus差价与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
+// 引入自定义插件对象：注册整个项目的全局组件
+import gloablComponents from '@/components/index';
+// 获取应用对象实例
+const app = createApp(App)
+// 安装element-plus插件
+app.use(ElementPlus, {
+    locale: zhCn //element-plus 默认是英文，这一步是国际化配置（中文）
+})
+// 安装自定义插件
+app.use(gloablComponents);
+// 将应用挂载到挂载点上
+app.mount('#app')
